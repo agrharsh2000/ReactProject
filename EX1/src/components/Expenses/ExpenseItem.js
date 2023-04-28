bff89b19e8a4b4c7e68c8adc3bae45f051ce313a
Harsh <agr.harsh.2000@gmail.com>
2023-04-28 11:50:07 +0530

    added functionality filtering of tasks with year

--- EX1/src/components/Expenses/ExpenseItem.js
+++ EX1/src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,20 @@
-import "./ExpenseItem.css";
-import Card from "../UI/Card";
-import React, { useState } from "react";
-import ExpenseDate from "./ExpenseDate";
-const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+import React from "react";
 
-  const clickHandler = () => {
-    setTitle("updated!");
-    console.log(title);
-  };
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
